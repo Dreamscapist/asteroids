93d378a7f5ecd92f9f3c6e5e209544d878ecd307
Dreamscapist <vesa.p.p.anttila@gmail.com>
2025-06-19 16:49:37 +0300

    250619-1649: end of CH1

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import pygame
 from constants import *
 
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     game_started = True
     while game_started != False:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, 000)
+            
+        screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
 
 if __name__ == "__main__":
     main()
