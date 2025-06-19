1d6c5301be0a4a48497599e316d37682c555ad94
Dreamscapist <vesa.p.p.anttila@gmail.com>
2025-06-19 17:39:02 +0300

    250619-1739: created circleshape and player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
@@ -7,20 +8,17 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-    game_started = True
-    while game_started != False:
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
